8daf9c762873447935e64b00f3c2919be54bcc13
insistsu <947737976@qq.com>
2021-02-21 11:44:53 +0800

    deploy

---
+++ precache-manifest.b139605f1a67506e2f66c930a5fb08a5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9bd7333197638bf561c2",
+    "url": "/Show-the-bill/css/app.83703fde.css"
+  },
+  {
+    "revision": "5f32c2081113a1de4a35f689db3ef953",
+    "url": "/Show-the-bill/index.html"
+  },
+  {
+    "revision": "9bd7333197638bf561c2",
+    "url": "/Show-the-bill/js/app.bf7765a3.js"
+  },
+  {
+    "revision": "b289ac202b963efbf02c",
+    "url": "/Show-the-bill/js/chunk-vendors.e9addd71.js"
+  },
+  {
+    "revision": "4c19ae82e94d2bbdf641062559953321",
+    "url": "/Show-the-bill/manifest.json"
+  },
+  {
+    "revision": "1af5b078fbf54de3a548b3c04e486071",
+    "url": "/Show-the-bill/qrcode.png"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/Show-the-bill/robots.txt"
+  }
+]);
